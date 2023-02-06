59af7b6848640d42d7b2e216a26d486a8627ca0b
SilasEduardo <silasandrade94@gmail.com>
2023-02-06 19:57:16 -0300

    error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Formik, Field, Form } from 'formik';
+import './App.css'
 
 function App() {
   function onSubmit(value, actions){
     console.log("SUBMIT", value)
 
   }
+
+  function validate(value){
+    const errors = {}
+    if(!value.name) {
+      errors.name = 'Nome é obrigatorio'
+    }
+
+    if(!value.email) {
+      errors.email = 'Email é obrigatorio'
+    }
+
+    return errors
+  }
   return (
     <div className="App">
       <Formik
+      validate={validate}
       onSubmit={onSubmit}
       initialValues={
         {
@@ -16,15 +31,25 @@ function App() {
           email: ''
         }
       }
-      render={({values})=>(
+      render={({values, errors})=>(
         <Form >
         <div>
           <label>Nome</label>
           <Field name='name' type='text'/>
+          {errors.name && (
+            <span className='testError'>
+              {errors.name}
+            </span>
+          )}
         </div>
         <div>
           <label>Email</label>
           <Field name='email'type='email'/>
+          {errors.email && (
+            <span className='testError'>
+              {errors.email}
+            </span>
+          )}
         </div>
         <button type='submit'>enviar</button>
   
